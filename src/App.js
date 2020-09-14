fa6c06aa344f6effd13756449fc43933c417c4bc
Vivekanand R K <vivekanandrk2001@gmail.com>
2020-09-14 19:08:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import Icon from "./components/Icon"
-// hii
+
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
 
@@ -14,82 +13,82 @@ const itemArray = new Array(9).fill("empty")
 
 const App = () => {
 
-const [isCross, setIsCross] = useState(false)
-const [winMessage, setWinMessage] = useState("")
+  const [isCross, setIsCross] = useState(false)
+  const [winMessage, setWinMessage] = useState("")
 
-const relaodGame = () => {
-  setIsCross(false);
-  setWinMessage("");
-  itemArray.fill("empty", 0, 9)
-};
+  const relaodGame = () => {
+    setIsCross(false);
+    setWinMessage("");
+    itemArray.fill("empty", 0, 9)
+  };
 
-const checkIsWinner = () => {
-  if (
-    itemArray[0] === itemArray[1] &&
-    itemArray[0] === itemArray[2] && 
-    itemArray[0] !== "empty"
+  const checkIsWinner = () => {
+    if (
+      itemArray[0] === itemArray[1] &&
+      itemArray[0] === itemArray[2] && 
+      itemArray[0] !== "empty"
+      ) {
+      setWinMessage(`${itemArray[0]} wins`)
+    } else if (
+      itemArray[3] !== "empty" &&
+      itemArray[3] === itemArray[4] && 
+      itemArray[4] == itemArray[5]
     ) {
-    setWinMessage(`${itemArray[0]} wins`)
-  } else if (
-    itemArray[3] !== "empty" &&
-    itemArray[3] === itemArray[4] && 
-    itemArray[4] == itemArray[5]
-  ) {
-    setWinMessage(`${itemArray[3]} wins`)
-  } else if (
-    itemArray[6] !== "empty" &&
-    itemArray[6] === itemArray[7] && 
-    itemArray[7] === itemArray[8]
-  ) {
-    setWinMessage(`${itemArray[6]} wins`)
-  } else if (
-    itemArray[0] !== "empty" &&
-    itemArray[0] === itemArray[3] && 
-    itemArray[3] === itemArray[6]
-  ) {
-    setWinMessage(`${itemArray[0]} wins`)
-  } else if (
-    itemArray[1] !== "empty" &&
-    itemArray[1] === itemArray[4] && 
-    itemArray[4] === itemArray[7]
-  ) {
-    setWinMessage(`${itemArray[1]} wins`)
-  } else if (
-    itemArray[2] !== "empty" &&
-    itemArray[2] === itemArray[5] && 
-    itemArray[5] === itemArray[8]
-  ) {
-    setWinMessage(`${itemArray[2]} wins`)
-  } else if (
-    itemArray[0] !== "empty" &&
-    itemArray[0] === itemArray[4] && 
-    itemArray[4] === itemArray[8]
-  ) {
-    setWinMessage(`${itemArray[0]} wins`)
-  } else if (
-    itemArray[2] !== "empty" &&
-    itemArray[2] === itemArray[4] && 
-    itemArray[4] === itemArray[6]
-  ) {
-    setWinMessage(`${itemArray[2]} wins`)
+      setWinMessage(`${itemArray[3]} wins`)
+    } else if (
+      itemArray[6] !== "empty" &&
+      itemArray[6] === itemArray[7] && 
+      itemArray[7] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[6]} wins`)
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] === itemArray[3] && 
+      itemArray[3] === itemArray[6]
+    ) {
+      setWinMessage(`${itemArray[0]} wins`)
+    } else if (
+      itemArray[1] !== "empty" &&
+      itemArray[1] === itemArray[4] && 
+      itemArray[4] === itemArray[7]
+    ) {
+      setWinMessage(`${itemArray[1]} wins`)
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] === itemArray[5] && 
+      itemArray[5] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[2]} wins`)
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] === itemArray[4] && 
+      itemArray[4] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[0]} wins`)
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] === itemArray[4] && 
+      itemArray[4] === itemArray[6]
+    ) {
+      setWinMessage(`${itemArray[2]} wins`)
+    } 
   } 
-} 
 
-const changeItem = itemNumber => {
-  if (winMessage) {
-    return toast(winMessage, {type: "success" });
-  }
+  const changeItem = itemNumber => {
+    if (winMessage) {
+      return toast(winMessage, {type: "success" });
+    }
 
-  if (itemArray[itemNumber] === "empty" ) {
+    if (itemArray[itemNumber] === "empty" ) {
       itemArray[itemNumber] = isCross ? "cross" : "circle"
       setIsCross(!isCross)
-  } else {
-    return toast("already filled", {type: "error"})
-  }
+    } else {
+      return toast("already filled", {type: "error"})
+    }
 
-  checkIsWinner();
+    checkIsWinner();
 
-};
+  };
 
   return (
     <Container className="p-5">
